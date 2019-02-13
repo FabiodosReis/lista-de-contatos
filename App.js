d9d155381eaab9d5b33c099065514f3c759300db
Fabio Reis <fabio_dos_reis@outlook.com>
2019-02-13 13:06:31 -0200

    add header

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Header';
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Teste</Text>
-      </View>
+      <View>
+        <Header title="Pessoas"/>
+      </View>          
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'; 
+
+//componente funcional
+const Header = (props) => (
+
+    <View style={style.container}>
+        <Text style={style.title}>
+            { props.title }
+        </Text>
+    </View>
+);
+
+const style = StyleSheet.create({
+
+    container: {
+        marginTop: 25,
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent: 'center',    
+    },
+
+    title: {
+        fontSize: 50,
+        color: '#FFF',
+    }   
+});
+
+export default Header;
