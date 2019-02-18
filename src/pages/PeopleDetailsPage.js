ea3428b054f67616e48fdc5e7487cd23fc065534
Fabio Reis <fabio_dos_reis@outlook.com>
2019-02-18 17:43:10 -0300

    add tabela

--- src/pages/PeopleDetailsPage.js
+++ src/pages/PeopleDetailsPage.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+import Tableline from '../components/TableLine';
 
 export default class PeopleDetailsPage extends React.Component {
 
 
  render(){
 
+    const { people } = this.props.navigation.state.params;
+
     return (
-        <View>
-            <Text>People Detail Page</Text>
+        <View style={styles.container}>
+            <Image
+                style={styles.avatar}
+                source={{ uri: people.picture.large }}/>
+
+                <View style={ styles.detailContainer }>
+                    <Tableline label="Email:" content={people.email} />
+                    <Tableline label="Cidade:" content={people.location.city} />  
+                    <Tableline label="Estado:" content={people.location.state} /> 
+                    <Tableline label="Tel:" content={people.phone} /> 
+                    <Tableline label="Cel:" content={people.cell} />    
+                    <Tableline label="Nacionalidade:" content={people.nat} />                  
+                </View>                               
         </View>
+        
     );
  }
 
 }
+
+const styles = StyleSheet.create({    
+
+        avatar: {
+            aspectRatio: 1
+        },
+
+        container: {
+            padding: 15           
+        },
+
+        detailContainer: {
+            backgroundColor: '#e2f9ff',
+            marginTop: 20,
+            elevation: 1
+
+        }        
+    });
